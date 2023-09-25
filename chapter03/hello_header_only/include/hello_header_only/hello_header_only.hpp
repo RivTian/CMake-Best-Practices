4355829c3168a6bcf58c3325aafa6fa2414b1ac1
Koshkaaa <pengtianjun@sansi.com>
2023-09-25 15:58:11 +0800

    Update: Create Chapter-03

---
+++ chapter03/hello_header_only/include/hello_header_only/hello_header_only.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+namespace hello_header_only {
+    void print_hello(const std::string & name) {
+        std::cout << "Hello " << name << " from a header only library\n";
+    }
+}
---
+++ chapter03/hello_world_standalone/src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(int, char **) {
+    std::cout << "Welcome to chapter 3\n";
+}
